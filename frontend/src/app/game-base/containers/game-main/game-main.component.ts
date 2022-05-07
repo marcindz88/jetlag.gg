226110954c429a2c99bad5c71cc037b32f095486
Marcin Dziechciaruk <marcindz88@gmail.com>
2022-05-08 00:45:33 +0200

    [FRONT-END] Basic plane movement and steering

--- frontend/src/app/game-base/containers/game-main/game-main.component.ts
+++ frontend/src/app/game-base/containers/game-main/game-main.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NgtCameraOptions, NgtGLOptions } from '@angular-three/core/lib/types';
 import { PCFSoftShadowMap, WebGLShadowMap } from 'three';
+import { PlaneState } from '../../utils/models/game.types';
+import { DEFAULT_PLANE_STATE } from '../../utils/models/game.constants';
+import { DirectionEnum } from '../../utils/models/game.enums';
 
 @Component({
   selector: 'pg-game-main',
@@ -8,6 +11,11 @@ import { PCFSoftShadowMap, WebGLShadowMap } from 'three';
   styleUrls: ['./game-main.component.scss']
 })
 export class GameMainComponent {
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.handleChangeOfDirection(event.key);
+  }
+
   readonly cameraOptions: NgtCameraOptions = {
     zoom: 1,
     position: [0, 15, 50]
@@ -19,4 +27,21 @@ export class GameMainComponent {
     enabled: true,
     type: PCFSoftShadowMap
   };
+
+  myPlaneState: PlaneState = DEFAULT_PLANE_STATE;
+
+
+  handleChangeOfDirection(keyCode: string) {
+    switch (keyCode) {
+      case 'ArrowLeft':
+        this.myPlaneState.direction = DirectionEnum.LEFT;
+        break;
+      case 'ArrowRight':
+        this.myPlaneState.direction = DirectionEnum.RIGHT;
+        break;
+      case 'ArrowUp':
+        this.myPlaneState.direction = DirectionEnum.FORWARD;
+        break;
+    }
+  }
 }
