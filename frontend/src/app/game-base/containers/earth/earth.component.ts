d7b5606d94c4be12e01563c35611066ae4575ceb
Marcin Dziechciaruk <marcin.dziechciaruk@pentacomp.pl>
2022-05-07 23:22:23 +0200

    [FRONT-END] Creation of a moving plane, update in textures handling

--- frontend/src/app/game-base/containers/earth/earth.component.ts
+++ frontend/src/app/game-base/containers/earth/earth.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { Color, FloatType, PMREMGenerator, TextureLoader, WebGLRenderer } from 'three';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
-import { NgtLoader } from '@angular-three/core';
-import { combineLatest, from, map } from 'rxjs';
+import { Color } from 'three';
+import { TextureModelsService } from '../../services/texture-models.service';
 
 @Component({
   selector: 'pg-earth',
@@ -10,24 +8,11 @@ import { combineLatest, from, map } from 'rxjs';
   styleUrls: ['./earth.component.scss']
 })
 export class EarthComponent {
-  textures$ = combineLatest([
-    this.ngtLoader.use(TextureLoader, 'assets/earthbump.jpg'),
-    this.ngtLoader.use(TextureLoader, 'assets/earthmap.jpg'),
-    this.ngtLoader.use(TextureLoader, 'assets/earthspec.jpg'),
-    this.fetchEnvMap()
-  ]).pipe(map(([bump, map, spec, env]) => ({ bump, map, spec, env })));
-
+  textures$ = this.textureModelsService.earthTextures$;
   sheenColor = new Color('#ff8a00').convertSRGBToLinear();
 
-  private pmrem = new PMREMGenerator(new WebGLRenderer());
-
-  constructor(private ngtLoader: NgtLoader) {
+  constructor(private textureModelsService: TextureModelsService) {
   }
 
-  fetchEnvMap() {
-    return from(new RGBELoader()
-      .setDataType(FloatType)
-      .loadAsync('assets/old_room_2k.hdr') // thanks to https://polyhaven.com/hdris !
-    ).pipe(map(envmapTexture => this.pmrem.fromEquirectangular(envmapTexture).texture));
-  }
+
 }
