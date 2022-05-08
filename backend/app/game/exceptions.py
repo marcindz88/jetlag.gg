b000f922f15ae5eca2a28ec8e376598c14d9ba7b
decomorreno <24738500+decomorreno@users.noreply.github.com>
2022-05-08 19:44:59 +0200

    Backend game session

---
+++ backend/app/game/exceptions.py
@@ -0,0 +1,14 @@
+class PlayerLimitExceeded(Exception):
+    pass
+
+
+class PlayerNotFound(Exception):
+    pass
+
+
+class PlayerAlreadyConnected(Exception):
+    pass
+
+
+class PlayerInvalidNickname(Exception):
+    pass
