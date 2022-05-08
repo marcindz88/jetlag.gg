51474641255adb3d29d76e0f005480c3f50a2524
Marcin Dziechciaruk <marcindz88@gmail.com>
2022-05-08 13:12:33 +0200

    [FRONT-END] Plane direction scale adjustment, speed handling

--- frontend/src/app/game-base/components/plane/plane.component.ts
+++ frontend/src/app/game-base/components/plane/plane.component.ts
@@ -3,7 +3,7 @@ import { TextureModelsService } from '../../utils/services/texture-models.servic
 import { NgtEuler, NgtRenderState, NgtVector3 } from '@angular-three/core';
 import { Group } from 'three';
 import { GeoLocationPoint } from '../../utils/models/game.types';
-import { DEFAULT_PLANE_STATE, EARTH_RADIUS } from '../../utils/models/game.constants';
+import { DEFAULT_PLANE_STATE, DIRECTION, EARTH_RADIUS, MAP_SCALE } from '../../utils/models/game.constants';
 import { degToRad } from 'three/src/math/MathUtils';
 import { calculateCircumference } from '../../utils/utils';
 
@@ -18,14 +18,25 @@ export class PlaneComponent {
     // TODO calculate starting rotation and position
   };
 
-  @Input() direction: number = DEFAULT_PLANE_STATE.direction;
+  @Input() set direction(direction: number) {
+    // Correct last direction if went out of scale
+    if (Math.abs(direction - this.lastDirection) > DIRECTION.max / 2) {
+      if (this.lastDirection > DIRECTION.max / 2) {
+        this.lastDirection -= DIRECTION.max;
+      } else {
+        this.lastDirection += DIRECTION.max;
+      }
+    }
+    // Update current direction
+    this.currentDirection = direction;
+  }
+
   @Input() speed = DEFAULT_PLANE_STATE.speed;
 
   private lastDirection: number = DEFAULT_PLANE_STATE.direction;
-
+  private currentDirection: number = DEFAULT_PLANE_STATE.direction;
   readonly textures$ = this.textureModelsService.planeTextures$;
 
-  readonly SPEED_DIVISOR = 100;
   readonly MOVING_RADIUS = EARTH_RADIUS + 1;
   readonly MOVING_CIRCUMFERENCE = calculateCircumference(this.MOVING_RADIUS);
 
@@ -37,22 +48,19 @@ export class PlaneComponent {
 
   updatePlane(event: { state: NgtRenderState; object: Group }) {
     // Update direction gradually
-    const currentDirectionDifference = this.direction - this.lastDirection;
+    let currentDirectionDifference = this.currentDirection - this.lastDirection;
+    // Rotate according to difference
     if (Math.abs(currentDirectionDifference) > 0.1) {
       const currentDifference = currentDirectionDifference / 30; // Divide change into 30 frames
       event.object.rotateZ(degToRad(currentDifference));
       this.lastDirection = this.lastDirection + currentDifference;
     } else {
+      // If difference very small - update lastdirection and make dinal rotation
       event.object.rotateZ(degToRad(currentDirectionDifference));
-      this.lastDirection = this.direction;
+      this.lastDirection = this.currentDirection;
     }
-
-    // Move forward by speed
-    event.object.rotateX(this.angle);
-    event.object.translateY(this.speed / this.SPEED_DIVISOR);
-  }
-
-  private get angle() {
-    return degToRad(this.speed / this.SPEED_DIVISOR / this.MOVING_CIRCUMFERENCE * 360);
+    // Move forward by speed and rotate downward to continue nosing down with curvature of earth
+    event.object.rotateX(degToRad(this.speed * MAP_SCALE / this.MOVING_CIRCUMFERENCE * 360));
+    event.object.translateY(this.speed * MAP_SCALE);
   }
 }
