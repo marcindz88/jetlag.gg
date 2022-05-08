57d3cd1e770c141fd6d692f060332a7dd2a809f6
Marcin Dziechciaruk <marcin.dziechciaruk@pentacomp.pl>
2022-05-08 13:12:33 +0200

    [FRONT-END] Plane direction scale adjustment, speed handling

--- frontend/src/app/game-base/utils/models/game.constants.ts
+++ frontend/src/app/game-base/utils/models/game.constants.ts
@@ -1,9 +1,28 @@
 import { PlaneState } from './game.types';
 
 export const EARTH_RADIUS = 40;
+export const REAL_EARTH_RADIUS = 6371; // km
+export const MAP_SCALE = EARTH_RADIUS / REAL_EARTH_RADIUS / 5000;
+
+
+// km/h
+export const SPEED = {
+  min: 100,
+  max: 10000,
+  step: 50,
+  default: 1000
+};
+
+// deg
+export const DIRECTION = {
+  min: 0,
+  max: 360,
+  step: 2,
+  default: 180
+};
 
 export const DEFAULT_PLANE_STATE: PlaneState = {
   initialPoint: { lat: 0, long: 0 },
-  speed: 2,
-  direction: 180
+  speed: SPEED.default,
+  direction: DIRECTION.default
 };
