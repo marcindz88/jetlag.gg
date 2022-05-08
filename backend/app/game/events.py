3aa22659838c6217f5df6546cbe994f4a45b3912
decomorreno <24738500+decomorreno@users.noreply.github.com>
2022-05-08 21:38:56 +0200

    Backend player related events

---
+++ backend/app/game/events.py
@@ -0,0 +1,26 @@
+import dataclasses
+import datetime
+import enum
+
+
+class EventType(str, enum.Enum):
+    PLAYER_CONNECTED = "player.connected"
+    PLAYER_REGISTERED = 'player.registered'
+    PLAYER_DISCONNECTED = 'player.disconnected'
+    PLAYER_REMOVED = 'player.removed'
+
+
+EVENTS_EMITTED_BY_SERVER = [
+    EventType.PLAYER_CONNECTED,
+    EventType.PLAYER_REGISTERED,
+    EventType.PLAYER_DISCONNECTED,
+    EventType.PLAYER_REMOVED,
+]
+
+
+@dataclasses.dataclass
+class Event:
+    type: EventType
+    data: dict
+    created: datetime.datetime = datetime.datetime.now()
+    emitted_by_server: bool = True
