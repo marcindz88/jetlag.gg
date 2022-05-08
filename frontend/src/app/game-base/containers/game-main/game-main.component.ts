76ea08809492bfbd060ab4131b02462cb04504d1
Marcin Dziechciaruk <marcindz88@gmail.com>
2022-05-08 13:12:33 +0200

    [FRONT-END] Plane direction scale adjustment, speed handling

--- frontend/src/app/game-base/containers/game-main/game-main.component.ts
+++ frontend/src/app/game-base/containers/game-main/game-main.component.ts
@@ -1,20 +1,16 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NgtCameraOptions, NgtGLOptions } from '@angular-three/core/lib/types';
 import { PCFSoftShadowMap, WebGLShadowMap } from 'three';
 import { PlaneState } from '../../utils/models/game.types';
-import { DEFAULT_PLANE_STATE } from '../../utils/models/game.constants';
+import { DEFAULT_PLANE_STATE, DIRECTION, SPEED } from '../../utils/models/game.constants';
+import { auditTime, filter, fromEvent, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'pg-game-main',
   templateUrl: './game-main.component.html',
   styleUrls: ['./game-main.component.scss']
 })
-export class GameMainComponent {
-  @HostListener('document:keydown', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-    this.handleChangeOfDirection(event.key);
-  }
-
+export class GameMainComponent implements OnDestroy {
   readonly cameraOptions: NgtCameraOptions = {
     zoom: 1,
     position: [0, 15, 50]
@@ -29,15 +25,52 @@ export class GameMainComponent {
 
   myPlaneState: PlaneState = DEFAULT_PLANE_STATE;
 
+  private destroy$ = new Subject<void>();
+  private keyDownEvent$ = fromEvent<KeyboardEvent>(document, 'keydown').pipe(takeUntil(this.destroy$));
 
-  handleChangeOfDirection(keyCode: string) {
-    switch (keyCode) {
-      case 'ArrowLeft':
-        this.myPlaneState.direction -= 5;
-        break;
-      case 'ArrowRight':
-        this.myPlaneState.direction += 5;
-        break;
-    }
+
+  constructor() {
+    this.setupDirectionChangeHandling();
+    this.setupSpeedChangeHandling();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+
+  private setupDirectionChangeHandling() {
+    this.keyDownEvent$
+      .pipe(filter(e => e.key === 'ArrowLeft'), auditTime(100))
+      .subscribe(() => {
+        this.myPlaneState.direction -= DIRECTION.step;
+        if (this.myPlaneState.direction < DIRECTION.min) {
+          this.myPlaneState.direction += DIRECTION.max;
+        }
+      });
+    this.keyDownEvent$
+      .pipe(filter(e => e.key === 'ArrowRight'), auditTime(100))
+      .subscribe(() => {
+        this.myPlaneState.direction += DIRECTION.step;
+        this.myPlaneState.direction %= DIRECTION.max;
+      });
+  }
+
+  private setupSpeedChangeHandling() {
+    this.keyDownEvent$
+      .pipe(filter(e => e.key === 'ArrowUp'), auditTime(100))
+      .subscribe(() => {
+        if (this.myPlaneState.speed + SPEED.step <= SPEED.max) {
+          this.myPlaneState.speed += SPEED.step;
+        }
+      });
+    this.keyDownEvent$
+      .pipe(filter(e => e.key === 'ArrowDown'), auditTime(100))
+      .subscribe(() => {
+        if (this.myPlaneState.speed - SPEED.step >= SPEED.min) {
+          this.myPlaneState.speed -= SPEED.step;
+        }
+      });
   }
 }
