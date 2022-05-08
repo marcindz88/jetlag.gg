e7bf13520a968ef8c80c0f3e5064a90d8c6b8955
decomorreno <24738500+decomorreno@users.noreply.github.com>
2022-05-09 00:07:21 +0200

    Backend add cors middleware

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, WebSocket, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, constr
 
 from app.game import exceptions
@@ -6,8 +7,18 @@ from app.game.core import GameSession
 from app.tools.websocket_server import StarletteWebsocketServer, WebSocketSession
 
 
-app = FastAPI()
 game_session = GameSession()
+app = FastAPI()
+
+origins = ["*"]  # todo
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/game/players/")
