e7adf36f725f1e0ed386600882f34d399af95c76
Marcin Dziechciaruk <marcin.dziechciaruk@pentacomp.pl>
2022-05-10 15:00:40 +0200

    [FRONT-END] Creation of coordinational logic and transformers

--- frontend/src/app/game-base/utils/models/game.constants.ts
+++ frontend/src/app/game-base/utils/models/game.constants.ts
@@ -4,6 +4,8 @@ export const EARTH_RADIUS = 40;
 export const REAL_EARTH_RADIUS = 6371; // km
 export const MAP_SCALE = EARTH_RADIUS / REAL_EARTH_RADIUS / 5000;
 
+export const FLIGHT_ALTITUDE = 100 * (EARTH_RADIUS / REAL_EARTH_RADIUS);
+
 // km/h
 export const SPEED = {
   min: 100,
@@ -21,7 +23,7 @@ export const DIRECTION = {
 };
 
 export const DEFAULT_PLANE_STATE: PlaneState = {
-  initialPoint: { lat: 0, long: 0 },
+  initialPoint: { lat: 52.22135563657265, long: 21.008107155957713 },
   speed: SPEED.default,
   direction: DIRECTION.default,
 };
