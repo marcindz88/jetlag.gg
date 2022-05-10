6a8be61aedc242302cfae399656fe05d656350a2
Marcin Dziechciaruk <marcin.dziechciaruk@pentacomp.pl>
2022-05-10 19:02:31 +0200

    [FRONT-END] Better keyboard handling, textures pre fetch

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TextureModelsService } from './game-base/utils/services/texture-models.service';
 
 @Component({
   selector: 'pg-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'plane-game';
+
+  constructor(private textureModelsService: TextureModelsService) {
+    this.textureModelsService.fetchAllTextures();
+  }
 }
