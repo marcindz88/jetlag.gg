628acbf2dcde5388b7fbd1a188bedb1ddb5847f8
Marcin Dziechciaruk <marcindz88@gmail.com>
2022-05-10 15:00:40 +0200

    [FRONT-END] Creation of coordinational logic and transformers

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MyPlayerGuard } from './players/guards/my-player.guard';
 import { ROUTES } from './shared/constants/routes';
 
 const routes: Routes = [
@@ -15,7 +14,7 @@ const routes: Routes = [
   },
   {
     path: ROUTES.game,
-    canLoad: [MyPlayerGuard],
+    // canLoad: [MyPlayerGuard],
     loadChildren: () => import('./game-base/game-base.module').then(m => m.GameBaseModule),
   },
   {
