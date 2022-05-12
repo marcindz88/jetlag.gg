bef2d75cd2657ebc3c25eedcc682a8079cc4d935
decomorreno <24738500+decomorreno@users.noreply.github.com>
2022-05-12 23:34:24 +0200

    Backend convert bearing value type from float to integer, allow small discrepancy when considering future events

--- backend/app/game/models.py
+++ backend/app/game/models.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, confloat, conint
+from pydantic import BaseModel, conint
 
 
 class PlayerPositionUpdateRequest(BaseModel):
-    bearing: confloat(ge=0, lt=360)
+    bearing: conint(ge=0, lt=360)
     velocity: conint(ge=0)
     timestamp: conint(ge=0)
