8fbc027928399b0db444cefa4338df566fac85db
Marcin Dziechciaruk <marcindz88@gmail.com>
2022-05-14 18:35:04 +0200

    [FRONT-END] Dynamic bearing

--- frontend/src/app/game-base/models/game.constants.ts
+++ frontend/src/app/game-base/models/game.constants.ts
@@ -5,7 +5,7 @@ export const EARTH_RADIUS = 40;
 export const REAL_EARTH_RADIUS = 6371; // km
 export const MAP_SCALE = EARTH_RADIUS / REAL_EARTH_RADIUS;
 
-export const FLIGHT_ALTITUDE = 100 * (EARTH_RADIUS / REAL_EARTH_RADIUS);
+export const FLIGHT_ALTITUDE = 200 * (EARTH_RADIUS / REAL_EARTH_RADIUS);
 export const CAMERA_ALTITUDE = FLIGHT_ALTITUDE + EARTH_RADIUS + 10;
 
 export const MOVING_RADIUS = EARTH_RADIUS + FLIGHT_ALTITUDE;
@@ -16,7 +16,7 @@ export const MOVING_CIRCUMFERENCE = calculateCircumference(MOVING_RADIUS);
 export const VELOCITY = {
   min: 0,
   max: 10000000,
-  step: 500000,
+  step: 50000,
   default: 2500000,
 };
 
