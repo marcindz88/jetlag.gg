f842e9294bbc6df7cd18f057196899297f3897bb
decomorreno <24738500+decomorreno@users.noreply.github.com>
2022-05-14 23:28:31 +0200

    Backend convert bearing to float

--- backend/app/game/models.py
+++ backend/app/game/models.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, conint
+from pydantic import BaseModel, conint, confloat
 
 
 class PlayerPositionUpdateRequest(BaseModel):
-    bearing: conint(ge=0, lt=360)
+    bearing: confloat(ge=0, lt=360)
     velocity: conint(ge=0)
     timestamp: conint(ge=0)
