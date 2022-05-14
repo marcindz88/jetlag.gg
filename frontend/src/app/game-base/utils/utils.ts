5ee15b6e0f6ebb16c95ab4203cd7edbe0484dd31
Marcin Dziechciaruk <marcindz88@gmail.com>
2022-05-14 17:22:08 +0200

    [FRONT-END] Keyboard handling fix, wrong coords fix, wrong bearing fix

--- frontend/src/app/game-base/utils/utils.ts
+++ frontend/src/app/game-base/utils/utils.ts
@@ -1,27 +1,26 @@
-import { NgtTriple } from '@angular-three/core/lib/types';
-import { Euler } from 'three';
+import { Euler, Vector3 } from 'three';
 import { degToRad, radToDeg } from 'three/src/math/MathUtils';
 
 import { GeoLocationPoint } from '../models/game.types';
 
 export const calculateCircumference = (radius: number) => 2 * Math.PI * radius;
 
-export const transformPointIntoCoordinates = ([x, y, z]: NgtTriple): GeoLocationPoint => {
+export const transformPointIntoCoordinates = ({ x, y, z }: Vector3): GeoLocationPoint => {
   return {
     lat: radToDeg(Math.atan2(y, Math.sqrt(Math.pow(x, 2) + Math.pow(z, 2)))),
     lon: -radToDeg(Math.atan2(z, x)),
   };
 };
 
-export const transformCoordinatesIntoPoint = (point: GeoLocationPoint, radius: number): NgtTriple => {
+export const transformCoordinatesIntoPoint = (point: GeoLocationPoint, radius: number): Vector3 => {
   const latRad = degToRad(point.lat);
   const longRad = degToRad(point.lon);
-  return [
+  return new Vector3(
     Math.cos(latRad) * Math.cos(longRad) * radius,
     Math.sin(latRad) * radius,
-    -Math.cos(latRad) * Math.sin(longRad) * radius,
-  ];
+    -Math.cos(latRad) * Math.sin(longRad) * radius
+  );
 };
 
 export const transformPointAndDirectionIntoRotation = (point: GeoLocationPoint, direction: number): Euler =>
-  new Euler(degToRad(point.lat), degToRad(270 + point.lon), degToRad(direction - 180), 'YXZ');
+  new Euler(degToRad(point.lat), degToRad(270 + point.lon), degToRad(direction), 'YXZ');
