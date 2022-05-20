8bd6b346d797d7eb0209b84cae6173f1dc5587b7
decomorreno <24738500+decomorreno@users.noreply.github.com>
2022-05-21 00:19:28 +0200

    Backend airports

---
+++ backend/app/game/consts.py
@@ -0,0 +1,12 @@
+from app.game.coordinates import Coordinates
+
+AIRPORTS = [
+    ("[DXB] DUBAI", Coordinates(25.252777, 55.364445)),
+    ("[FRA] FRANKFURT", Coordinates(50.033333, 8.570556)),
+    ("[CDG] PARIS", Coordinates(49.009724, 2.547778)),
+    ("[HKG] HONG KONG", Coordinates(22.308046, 113.918480)),
+    ("[ANC] ANCHORAGE", Coordinates(61.171519, -149.990777)),
+    ("[PVG]	SHANGHAI", Coordinates(31.143333, 121.805275)),
+    ("[MIA] MIAMI", Coordinates(25.7933, -80.2906)),
+    ("[LAX] LOS ANGELES", Coordinates(33.942791, -118.410042)),
+]
