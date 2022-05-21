2e1e02d3d98bdee4a23be22f3f770fd0d2c2c3cb
Marcin Dziechciaruk <marcin.dziechciaruk@pentacomp.pl>
2022-05-21 17:50:40 +0200

    [FRONT-END] Compass implementation and styling fixes

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NgtRepeatModule } from '@angular-three/core';
 import { CardComponent } from '@shared/components/card/card.component';
 import { LoaderComponent } from '@shared/components/loader/loader.component';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, NgtRepeatModule],
   declarations: [CardComponent, LoaderComponent],
   exports: [CardComponent, LoaderComponent],
 })
