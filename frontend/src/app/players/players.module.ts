1cb51c96eaa078b0fb29432df59f367e5d932e58
Marcin Dziechciaruk <marcindz88@gmail.com>
2022-05-21 17:50:40 +0200

    [FRONT-END] Compass implementation and styling fixes

--- frontend/src/app/players/players.module.ts
+++ frontend/src/app/players/players.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { PlayerStatsComponent } from '@pg/players/components/player-stats/player-stats.component';
+import { NgtRepeatModule } from '@angular-three/core';
+import { CompassComponent } from '@pg/players/components/compass/compass.component';
+import { PlayerCockpitComponent } from '@pg/players/components/player-cockpit/player-cockpit.component';
 import { SharedModule } from '@shared/shared.module';
 
 import { PlayerSummaryComponent } from './components/player-summary/player-summary.component';
 import { PlayersSummaryComponent } from './components/players-summary/players-summary.component';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule, SharedModule],
-  declarations: [PlayersSummaryComponent, PlayerSummaryComponent, PlayerStatsComponent],
-  exports: [PlayersSummaryComponent, PlayerStatsComponent],
+  imports: [CommonModule, HttpClientModule, SharedModule, NgtRepeatModule],
+  declarations: [PlayersSummaryComponent, PlayerSummaryComponent, PlayerCockpitComponent, CompassComponent],
+  exports: [PlayersSummaryComponent, PlayerCockpitComponent],
 })
 export class PlayersModule {}
