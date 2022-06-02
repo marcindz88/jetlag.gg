c5645a9232b739c74a0084fff8a9cb17c5310d47
Marcin Dziechciaruk <marcindz88@gmail.com>
2022-06-02 23:51:14 +0200

    [FRONT-END] Styling fixes - icons, airport panel padding, snackbar increased period

--- frontend/src/app/shared/components/notification/notification-snackbar.config.ts
+++ frontend/src/app/shared/components/notification/notification-snackbar.config.ts
@@ -2,7 +2,7 @@ import { Provider } from '@angular/core';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 export const notificationSnackbarConfig: MatSnackBarConfig = {
-  duration: 4000,
+  duration: 10000,
   horizontalPosition: 'center',
   verticalPosition: 'top',
   panelClass: 'notification-snack-bar-container',
