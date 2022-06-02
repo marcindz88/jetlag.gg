5720060249c4a5371819923f11205abb386a2d62
decomorreno <24738500+decomorreno@users.noreply.github.com>
2022-06-02 23:35:44 +0200

    Backend increase package count & bot bug fix

--- backend/app/game/config.py
+++ backend/app/game/config.py
@@ -5,7 +5,7 @@ import dataclasses
 class GameConfig:
     MAX_PLAYERS: int = 16
     PLAYER_TIME_TO_CONNECT: int = 5000  # 5 seconds
-    MAX_SHIPMENTS_IN_GAME: int = 40
+    MAX_SHIPMENTS_IN_GAME: int = 60
     MIN_VELOCITY: int = 0
     MAX_VELOCITY: int = 2_000_000  # 2M km/h
     AIRPORT_MAXIMUM_DISTANCE_TO_LAND: int = 500  # 500km
