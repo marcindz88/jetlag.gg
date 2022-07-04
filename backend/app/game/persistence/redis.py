38a8ca8b54f19992af39f3cd4159012b857138fb
decomorreno <24738500+decomorreno@users.noreply.github.com>
2022-07-04 20:06:25 +0200

    Replace redisearch with standard redis datatypes - hash and sorted set

--- backend/app/game/persistence/redis.py
+++ backend/app/game/persistence/redis.py
@@ -1,12 +1,8 @@
-import json
 import uuid
 from typing import Optional
 
 import redis
 from redis.commands.json.path import Path
-from redis.commands.search.field import TagField, NumericField
-from redis.commands.search.indexDefinition import IndexDefinition, IndexType
-from redis.commands.search.query import Query
 
 from app.game.persistence.base import BasePersistentStorage, Player, PlayerList
 
@@ -46,44 +42,38 @@ class RedisPersistentStorage(BasePersistentStorage):
         nickname_number = self.client.incr(f"pnum:{nickname}")
         full_nickname = f"{nickname}:{nickname_number}"
         self.client.json().set(f'player:{full_nickname}', Path.root_path(), player)
+        self.client.hset("tokens", token, full_nickname)
+        self.client.zadd("player_score_index", {full_nickname: player['best_score']})
 
         return self._parse_player(nickname=full_nickname, player=player)
 
     def get_player_by_token(self, token: str) -> Optional[Player]:
-        try:
-            result = self.client.ft("idx:players_json").search("@token:{ %s }" % token)
-        except redis.ResponseError:
+        full_nickname = self.client.hget("tokens", token)
+        if not full_nickname:
             return None
 
-        if result.total != 1:
-            return None
-
-        document = result.docs[0]
-
-        nickname = document.id.split(":", 1)[1]
-        player = json.loads(document.json)
-
-        if player['token'] != token:
-            # lets make sure nobody did i.e. "8a*"
-            return None
-
-        return self._parse_player(nickname=nickname, player=player)
+        player = self.client.json().get(f"player:{full_nickname}")
+        return self._parse_player(nickname=full_nickname, player=player)
 
     def get_player_list(self, limit: int, offset: int) -> PlayerList:
         if limit <= 0 or offset < 0:
             raise ValueError
 
-        print("get_player_list")
+        total = self.client.zcard("player_score_index")
+        if total == 0:
+            return PlayerList(
+                total=total,
+                results=[],
+            )
 
-        query = Query(query_string="*").sort_by("best_score", asc=False).paging(offset=offset, num=limit)
-        results = self.client.ft("idx:players_json").search(query)
-
-        print("get_player_list 2")
+        full_names = self.client.zrevrange("player_score_index", offset, offset + limit)
+        keys = [f"player:{name}" for name in full_names]
+        players = self.client.json().mget(path=Path.root_path(), keys=keys)
 
         return PlayerList(
-            total=results.total,
+            total=total,
             results=[
-                self._parse_player(nickname=d.id.split(":", 1)[1], player=json.loads(d.json)) for d in results.docs
+                self._parse_player(nickname=full_names[i], player=players[i]) for i in range(len(keys))
             ],
         )
 
@@ -103,43 +93,19 @@ class RedisPersistentStorage(BasePersistentStorage):
 
         player_key = f'player:{full_nickname}'
 
-        def _update_best_score(pipe):
+        def _add_game_record(pipe):
             player = pipe.json().get(player_key)
+            pipe.multi()
+            pipe.json().set(f"game:{full_nickname}:{timestamp}", Path.root_path(), game)
             if score <= player['best_score']:
                 return
-            pipe.multi()
             pipe.json().set(player_key, Path("best_score"), score)
             pipe.json().set(player_key, Path("best_shipment_num"), shipments_delivered)
             pipe.json().set(player_key, Path("best_time_alive"), time_alive)
             pipe.json().set(player_key, Path("best_timestamp"), timestamp)
-            pipe.json().set(f"game:{full_nickname}:{timestamp}", Path.root_path(), game)
+            pipe.zadd("player_score_index", {full_nickname: score})
 
-        self.client.transaction(_update_best_score, player_key)
+        self.client.transaction(_add_game_record, player_key)
 
     def clear_db(self):
         self.client.flushdb()
-        self.rebuild_index()
-
-    def rebuild_index(self):
-        index_name = "idx:players_json"
-        try:
-            self.client.ft(index_name).dropindex()
-        except redis.ResponseError:
-            pass
-
-        index_schema = (
-            TagField(
-                '$.token',
-                as_name='token',
-            ),
-            NumericField(
-                '$.best_score',
-                as_name='best_score',
-                sortable=True,
-            ),
-        )
-
-        self.client.ft(index_name).create_index(
-            index_schema,
-            definition=IndexDefinition(index_type=IndexType.JSON, prefix=["player:"])
-        )
--- backend/scripts.py
+++ backend/scripts.py
@@ -1,11 +1,6 @@
 from app.game.persistence.redis import RedisPersistentStorage
 
 
-def rebuild_index():
-    storage = RedisPersistentStorage()
-    storage.rebuild_index()
-
-
 def clear_db():
     storage = RedisPersistentStorage()
     storage.clear_db()
